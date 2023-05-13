a323ee30e27cb517a72d14ac0f9f6ee39f8401f3
Vinicius Viana <92409125+vininiciusviana@users.noreply.github.com>
2023-05-13 14:16:10 -0300

    Adicionado os Botões

--- Project1.dpr
+++ Project1.dpr
@@ -2,13 +2,13 @@ program Project1;
 
 uses
   Vcl.Forms,
-  Unit1 in 'Unit1.pas' {Form1};
+  Unit1 in 'Unit1.pas' {Form_Principal};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TForm_Principal, Form_Principal);
   Application.Run;
 end.
--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,31 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls;
 
 type
-  TForm1 = class(TForm)
+  TForm_Principal = class(TForm)
+    P_central: TPanel;
+    P_tela: TPanel;
+    P_barra: TPanel;
+    P_barra2: TPanel;
+    P_barra3: TPanel;
+    B_9: TButton;
+    B_8: TButton;
+    B_7: TButton;
+    B_dividir: TButton;
+    B_6: TButton;
+    B_5: TButton;
+    B_4: TButton;
+    B_multip: TButton;
+    B_3: TButton;
+    B_2: TButton;
+    B_1: TButton;
+    B_subtr: TButton;
+    B_0: TButton;
+    B_virg: TButton;
+    B_result: TButton;
+    B_adicao: TButton;
   private
     { Private declarations }
   public
@@ -15,7 +36,7 @@ type
   end;
 
 var
-  Form1: TForm1;
+  Form_Principal: TForm_Principal;
 
 implementation
 
